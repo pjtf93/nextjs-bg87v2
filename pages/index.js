c6e422ff7fc8d1741609ad30dce52274685723c5
pjtf93 <pjtf93@gmail.com>
2021-05-20 15:03:21 -0400

    1 file modified

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,8 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js! thesting thiss</a>
+          Welcome to{' '}
+          <a href="https://nextjs.org">Next.js! How to make a commit</a>
         </h1>
 
         <p className={styles.description}>
